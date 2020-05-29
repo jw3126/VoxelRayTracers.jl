30058e9095c40c71c4744f1a9620991bde67673c
efmanu <manuedavakandam@gmail.com>
2020-05-29 11:18:28 +1000

    simple fix to avoid error while working with AD

--- src/core.jl
+++ src/core.jl
@@ -35,7 +35,7 @@ function EachTraversal(edges::NTuple{N, AbstractVector}, position::AbstractVecto
         end
     end
     signs = map(velocity) do v
-        Int(sign(v))
+        v > 0 ? 1 : -1
     end
     E = typeof(edges)
     EachTraversal{N,T,E}(edges, position, velocity, invvelocity, signs)
